7540ec249644d2cd45539911163a39670cb32ff2
nicholas570 <nicolas.mercier570@gmail.com>
2020-06-24 20:39:44 +0200

    displaying patients onclick

--- src/views/DashboardMedecin.jsx
+++ src/views/DashboardMedecin.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const patients = ['uno', 'dos', 'tres', 'unpasito', 'palante', 'maria'];
 
 const DashboardMedecin = () => {
-  return(
+  const [display, setDisplay] = useState(false);
+
+  const handleClickPatients = (e) => {
+    setDisplay(!display);
+  };
+
+  return (
     <>
-    <h2>DashboardMedecin</h2>
+      <h2>DashboardMedecin</h2>
+      <div>Date/heure</div>
+      <div>
+        <button onClick={() => handleClickPatients()}>Patients</button>
+        <div>{display && patients.map((patient) => patient)}</div>
+      </div>
+      <button>Create prescription</button>
+      <input placeholder='add a medoc' />
+      <button>Create patient</button>
     </>
   );
-}
+};
 
 export default DashboardMedecin;
