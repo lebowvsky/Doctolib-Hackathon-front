2b4ad4b812236dbf4bb03541eb93d1048efdd437
nicholas570 <nicolas.mercier570@gmail.com>
2020-06-25 01:04:54 +0200

    displaying past and current prescriptions

--- src/views/ButtonNow.jsx
+++ src/views/ButtonNow.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 
 import style from './patient.module.css';
 
+const CURRENT_PRESCRIPTIONS = [
+  { name: 'dsrfse' },
+  { name: 'hserze' },
+  { name: 'afeqdz' },
+];
+
 export default function ButtonNow() {
   const [display, setDisplay] = useState(false);
 
@@ -9,11 +15,16 @@ export default function ButtonNow() {
     setDisplay(!display);
   };
 
+  const prescriptions = CURRENT_PRESCRIPTIONS.map((item) => {
+    return <p key={item.name}>{item.name}</p>;
+  });
+
   return (
     <div
       className={display ? style.open : style.close}
       onClick={() => handleDisplay()}>
       now
+      {display && prescriptions}
     </div>
   );
 }
--- src/views/ButtonPast.jsx
+++ src/views/ButtonPast.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 
 import style from './patient.module.css';
 
+const PAST_PRESCRIPTIONS = [
+  { name: 'dsrfse' },
+  { name: 'hserze' },
+  { name: 'afeqdz' },
+];
+
 export default function ButtonPast() {
   const [display, setDisplay] = useState(false);
 
@@ -9,11 +15,18 @@ export default function ButtonPast() {
     setDisplay(!display);
   };
 
+  const prescriptions = PAST_PRESCRIPTIONS.map((item) => {
+    return <p key={item.name}>{item.name}</p>;
+  });
+
   return (
-    <div
-      className={display ? style.open : style.close}
-      onClick={() => handleDisplay()}>
-      past
-    </div>
+    <>
+      <div
+        className={display ? style.open : style.close}
+        onClick={() => handleDisplay()}>
+        past
+        {display && prescriptions}
+      </div>
+    </>
   );
 }
