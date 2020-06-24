c6e10e9bbc853e7f430fb6bda36d94a2d2c44faf
erwanitwish <quillec.erwan@gmail.com>
2020-06-25 01:50:53 +0200

    near last design

--- src/views/DashboardPatient.jsx
+++ src/views/DashboardPatient.jsx
@@ -12,44 +12,60 @@ const DashboardPatient = () => {
   };
 
   return (
-    <div className={styles.dashboard}>
-      <div className={styles.topPage}>
-        <h2>My Dashboard</h2>
-        <Clock />
-      </div>
-      <div
-        onClick={() => handleClick()}
-        className={isOpen ? styles.open : styles.close}
-      >
-        <div
-          className={
-            isOpen
-              ? styles.prescriptionInfosOpen
-              : styles.prescriptionInfosClose
-          }
-        >
-          <p className={styles.prescriptionTitle}>My prescriptions</p>
-          <p className={styles.prescriptionNumber}>3</p>
+    <div className={styles.container}>
+      <div className={styles.dashboard}>
+        <div className={styles.topPage}>
+          <h2>My Dashboard</h2>
+          <Clock />
         </div>
-        <div className={isOpen ? styles.contentOpen : styles.contentClose}>
-          <div>
-            <p className={styles.prescriptionName}>Ordonnance 1</p>
-            <p className={styles.prescriptionDescription}>
-              petite description pour voir de quoi ça parle
-            </p>
+        <div
+          onClick={() => handleClick()}
+          className={isOpen ? styles.open : styles.close}
+        >
+          <div
+            className={
+              isOpen
+                ? styles.prescriptionInfosOpen
+                : styles.prescriptionInfosClose
+            }
+          >
+            <p className={styles.prescriptionTitle}>My prescriptions</p>
+            <p className={styles.prescriptionNumber}>3</p>
           </div>
-          <div>
-            <p className={styles.prescriptionName}>Ordonnance 2</p>
-            <p className={styles.prescriptionDescription}>petite description</p>
+          <div className={isOpen ? styles.contentOpen : styles.contentClose}>
+            <div>
+              <p className={styles.prescriptionName}>Ordonnance 1</p>
+              <p className={styles.prescriptionDescription}>
+                petite description pour voir de quoi ça parle
+              </p>
+            </div>
+            <div>
+              <p className={styles.prescriptionName}>Ordonnance 2</p>
+              <p className={styles.prescriptionDescription}>
+                petite description
+              </p>
+            </div>
+            <div>
+              <p className={styles.prescriptionName}>Ordonnance 3</p>
+              <p className={styles.prescriptionDescription}>
+                petite description
+              </p>
+            </div>
+            <div>
+              <p className={styles.prescriptionName}>Ordonnance 4</p>
+              <p className={styles.prescriptionDescription}>
+                petite description
+              </p>
+            </div>
           </div>
-          <div>
-            <p className={styles.prescriptionName}>Ordonnance 3</p>
-            <p className={styles.prescriptionDescription}>petite description</p>
-          </div>
-          <div>
-            <p className={styles.prescriptionName}>Ordonnance 4</p>
-            <p className={styles.prescriptionDescription}>petite description</p>
-          </div>
+        </div>
+        <div className={styles.reminder}>
+          <p className={styles.notifNumber}>8</p>
+          <p className={styles.notifTitle}>Next notifs</p>
+        </div>
+        <div className={styles.new}>
+          <p className={styles.notifNumber}>+</p>
+          <p className={styles.notifTitle}>Add new</p>
         </div>
       </div>
       <div onClick='' className={styles.drugHistory}>
