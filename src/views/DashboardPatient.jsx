c535536311c3f6fbef40a713893806268a4e82a8
erwanitwish <quillec.erwan@gmail.com>
2020-06-25 00:59:15 +0200

    fake prescriptions added

--- src/views/DashboardPatient.jsx
+++ src/views/DashboardPatient.jsx
@@ -17,13 +17,40 @@ const DashboardPatient = () => {
         <h2>My Dashboard</h2>
         <Clock />
       </div>
-
       <div
         onClick={() => handleClick()}
         className={isOpen ? styles.open : styles.close}
       >
-        <p className={styles.prescriptionTitle}>My prescriptions</p>
-        <p className={styles.prescriptionNumber}>3</p>
+        <div
+          className={
+            isOpen
+              ? styles.prescriptionInfosOpen
+              : styles.prescriptionInfosClose
+          }
+        >
+          <p className={styles.prescriptionTitle}>My prescriptions</p>
+          <p className={styles.prescriptionNumber}>3</p>
+        </div>
+        <div className={isOpen ? styles.contentOpen : styles.contentClose}>
+          <div>
+            <p className={styles.prescriptionName}>Ordonnance 1</p>
+            <p className={styles.prescriptionDescription}>
+              petite description pour voir de quoi ça parle
+            </p>
+          </div>
+          <div>
+            <p className={styles.prescriptionName}>Ordonnance 2</p>
+            <p className={styles.prescriptionDescription}>petite description</p>
+          </div>
+          <div>
+            <p className={styles.prescriptionName}>Ordonnance 3</p>
+            <p className={styles.prescriptionDescription}>petite description</p>
+          </div>
+          <div>
+            <p className={styles.prescriptionName}>Ordonnance 4</p>
+            <p className={styles.prescriptionDescription}>petite description</p>
+          </div>
+        </div>
       </div>
       <div onClick='' className={styles.drugHistory}>
         <p>My drug history</p>
