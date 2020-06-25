e0b941ed688d4ecdda18933f1e1ea1aa67fc436f
Tobii777 <robin.dorion@gmail.com>
2020-06-25 16:29:29 +0200

    Adding media icons

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Home.module.css';
+import doctorIcon from '../medias/doctor-02.svg';
+import patientIcon from '../medias/patient-01.svg';
 
 const Home = () => {
 
@@ -11,13 +13,17 @@ const Home = () => {
       <div className={styles.ButtonDiv}>
         <div className={styles.ButtonSubDiv}>
           <Link to="/dashboard-medecin">
-            <div className={styles.Button} />
+            <div className={styles.Button}>
+              <img class={styles.ButtonImg} src={doctorIcon} alt="Doctor icon"/> 
+            </div>
           </Link>
           <p>Doctor</p>
         </div>
         <div className={styles.ButtonSubDiv}>
           <Link to="/dashboard-patient">
-            <div className={styles.Button}/>
+            <div className={styles.Button}>
+              <img className={styles.ButtonImg} src={patientIcon} alt="Patient icon"/>
+            </div>
           </Link>
           <p>Patient</p>
         </div>
