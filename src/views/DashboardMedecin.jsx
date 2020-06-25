ccb4a392a8efb8d7f8e3d28c79b1754a4be47513
nicholas570 <nicolas.mercier570@gmail.com>
2020-06-25 10:29:21 +0200

    adding a drug

--- src/views/DashboardMedecin.jsx
+++ src/views/DashboardMedecin.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import axios from 'axios';
+
 const DashboardMedecin = () => {
+  const [medocToAdd, setMedocToAdd] = useState();
+
+  const addMedocOnHooks = (e) => {
+    setMedocToAdd(e.target.value);
+  };
+
+  const handleAddMedoc = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:8080/api/produits', { nom: medocToAdd })
+      .then((res) => res.data)
+      .then((res) => {
+        alert('Medoc ajouté');
+      })
+      .catch((e) => {
+        console.error(e);
+        alert(`Erreur lors de l'ajout du produit : ${e.message}`);
+      });
+  };
   return (
     <>
       <h2>DashboardMedecin</h2>
@@ -12,7 +33,21 @@ const DashboardMedecin = () => {
         </Link>
       </div>
       <button>Create prescription</button>
-      <input placeholder='add a medoc' />
+      <div id='medocs'>
+        <form id='add_medoc' onSubmit={handleAddMedoc}>
+          <div>
+            <label htmlFor='new_medoc_name'>drug's name</label>
+            <input
+              type='text'
+              name='new_medoc_name'
+              id='new_medoc_name'
+              placeholder="drug's name"
+              onChange={addMedocOnHooks}
+            />
+          </div>
+          <button type='submit'>Add Drug</button>
+        </form>
+      </div>
       <button>Create patient</button>
     </>
   );
