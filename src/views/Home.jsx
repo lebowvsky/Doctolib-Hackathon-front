76dcc3340082c6d9b06cc54751f185d41f736a7d
Tobii777 <robin.dorion@gmail.com>
2020-06-25 16:45:06 +0200

    Adding logo on home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,12 +4,18 @@ import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
 import doctorIcon from '../medias/doctor-02.svg';
 import patientIcon from '../medias/patient-01.svg';
+import LogoVertical from '../medias/logo-verticalG-01.svg';
+import LogoHorizontal from '../medias/logo-horizontalG-02.svg';
+import LogoMono from '../medias/logo-mono-03.svg';
 
 const Home = () => {
 
   return(
     <div className={styles.MainDiv}>
-      <div className={styles.LogoDiv}><h2>LOGO</h2></div>
+      <div className={styles.LogoDiv}>
+        <img src={LogoHorizontal} alt="App Logo" />
+        <h2 className={styles.LogoText}>Louisette rase tout</h2>
+      </div>
       <div className={styles.ButtonDiv}>
         <div className={styles.ButtonSubDiv}>
           <Link to="/dashboard-medecin">
