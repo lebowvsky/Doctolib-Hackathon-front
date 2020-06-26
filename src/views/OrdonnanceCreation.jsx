86ccfb3960366f3e132ddbf606673dc778f75abc
nicholas570 <nicolas.mercier570@gmail.com>
2020-06-26 13:37:56 +0200

    form done

--- src/views/OrdonnanceCreation.jsx
+++ src/views/OrdonnanceCreation.jsx
@@ -276,32 +276,33 @@ const OrdonnanceCreation = (props) => {
 
       {/* MEDOC */}
       <div className={showAll ? styles.showAll : styles.noShowAll}>
-        <div className={styles.FlexDiv}>
-          <form
-            id='add_medoc'
-            className={styles.MarginDiv}
-            onSubmit={handleAddMedoc}>
-            <label className={styles.text} htmlFor='new_medoc_name'>
-              Add a new drug
-            </label>
-            <input
-              className={styles.Form}
-              type='text'
-              name='new_medoc_name'
-              id='new_medoc_name'
-              placeholder="drug's name"
-              onChange={addMedocOnHooks}
-            />
-            <button className={styles.AddPatientButton} type='submit'>
-              Add Drug
-            </button>
-          </form>
-        </div>
         <div id='ordonnance'>
           <h3
+            id={styles.blueName}
             className={
               styles.title
             }>{`${patientSelectedFirstname} ${patientSelectedLastname}`}</h3>
+          <div className={styles.FlexDiv}>
+            <form
+              id='add_medoc'
+              className={styles.MarginDiv}
+              onSubmit={handleAddMedoc}>
+              <label className={styles.text} htmlFor='new_medoc_name'>
+                <h3 className={styles.title}>Create a new drug</h3>
+              </label>
+              <input
+                className={styles.Form}
+                type='text'
+                name='new_medoc_name'
+                id='new_medoc_name'
+                placeholder="drug's name"
+                onChange={addMedocOnHooks}
+              />
+              <button className={styles.AddPatientButton} type='submit'>
+                Create new drug
+              </button>
+            </form>
+          </div>
           <Medoc
             medocAll={medocAll}
             handleSubmitCommande={handleSubmitCommande}
